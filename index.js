f06d7c454ce4d1ac5342b709b9869adc20635479
Will Omonaye <96916426+Omonayeg@users.noreply.github.com>
2022-06-21 20:30:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var name = "Will Omonaye.";
+var height = "6 feet.";
+var country = "Nigeria.";
+
+document.getElementById("name").innerHTML = "Name: " + name;
+document.getElementById("height").innerHTML = "Height: " + height;
+document.getElementById("country").innerHTML = "Country: " + country;
